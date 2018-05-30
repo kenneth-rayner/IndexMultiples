cd600d7365021de31ca9ca3227997fe1d1502bfd
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-30 07:21:14 +0100

    refactored code

--- src/main/scala/HeadsAndLegs.scala
+++ src/main/scala/HeadsAndLegs.scala
@@ -1,17 +1,11 @@
 object HeadsAndLegs {
 
   def chickensAndCows(heads: Int, legs: Int): Any = {
-
-    if (legs % 2 == 0) {
-      val numCows = (legs - (2 * heads)) / 2
-      val numChickens = heads - numCows
-
-      if ((numChickens >= 0) && (numCows >= 0)) {
-        (numChickens,numCows)
-      }
-      else
-        "No solution found"
+    val numCows = (legs - (2 * heads)) / 2
+    val numChickens = heads-numCows
+    legs match {
+        case x if (x % 2 == 0) && (numChickens >= 0) && (numCows >= 0) =>(numChickens,numCows)
+        case _ =>"No solution found"
     }
-    else "No solution found"
   }
 }
--- src/main/scala/index multiples.sc
+++ src/main/scala/index multiples.sc
@@ -1,7 +1,2 @@
-def candch(legs:Int=2,heads:Int=6):
 
-val numChicks = ((4 * heads) - legs) / 2
-  val numCows = (legs - (2 * heads)) / 2
-
-  if ((numChicks >= 0) && (numCows >= 0))
 
